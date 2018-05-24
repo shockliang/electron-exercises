f0c690281f2f92570122aa67b204ab618c574cc4
shocklaing <shockwater@gmail.com>
2018-05-24 22:34:36 +0800

    Initial pomodoro clock app.

---
+++ pomodoro-clock/bootstrap.js
@@ -0,0 +1,18 @@
+const electron = require("electron");
+const app = electron.app;
+const path = require("path");
+const url = require("url");
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ width: 600, height: 1000 });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+});
