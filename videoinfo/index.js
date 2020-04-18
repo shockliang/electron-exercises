eaf77581ac721ef1a9eec708a79bded7208b14de
shockliang <shockwater@gmail.com>
2020-04-19 00:21:38 +0800

    Video metadata wit FFProbe.

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,12 +1,15 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 const {app, BrowserWindow, ipcMain} = electron;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({webPreferences:{nodeIntegration: true}});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', () => {
-
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata)=> {
+    console.log(metadata);
+  });
 })
 
