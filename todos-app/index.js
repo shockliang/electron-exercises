c2b096ce5a33c9eddafa9a24d6744b3a404b59d8
shockliang <shockwater@gmail.com>
2020-04-22 00:26:40 +0800

    Constructing menu bar.

--- todos-app/index.js
+++ todos-app/index.js
@@ -1,9 +1,21 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({webPreferences: {nodeIntegration: true}});
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {label: 'New Todo'}
+    ]
+  }
+]
