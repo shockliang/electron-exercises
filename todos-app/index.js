81b99adfc87d50e8d6609d6d2f6e1bc5f502f7f3
shockliang <shockwater@gmail.com>
2020-04-25 01:17:57 +0800

    Creating separate windows.

--- todos-app/index.js
+++ todos-app/index.js
@@ -2,20 +2,32 @@ const electron = require('electron');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({webPreferences: {nodeIntegration: true}});
-  mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo',
+    webPreferences: {nodeIntegration: true}});
+}
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      {label: 'New Todo'},
+      {
+        label: 'New Todo',
+        click() {createAddWindow();}
+      },
       {
         label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
