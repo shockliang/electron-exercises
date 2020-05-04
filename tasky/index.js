5366f50493bd925bd599495812fb9172b147f8e2
shockliang <shockwater@gmail.com>
2020-05-05 00:06:06 +0800

    Detecting visibility.

--- tasky/index.js
+++ tasky/index.js
@@ -22,7 +22,11 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on('click', () =>{
-    mainWindow.show();
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
   })
 });
