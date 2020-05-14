b48c10ded68a94dda31510eb5b3e1d45228f8ff5
shockliang <shockwater@gmail.com>
2020-05-15 00:34:02 +0800

    BrowserWindow creation.

---
+++ convert/index.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      backgroundThrottling: false,
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+})
