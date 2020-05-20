8fef4c53a31754f65abfc3ce0412f7a387f91fef
shockliang <shockwater@gmail.com>
2020-05-21 00:06:24 +0800

    Batch video conversion.

--- convert/index.js
+++ convert/index.js
@@ -34,10 +34,13 @@ ipcMain.on('videos:added', (event, videos) => {
 
 ipcMain.on('conversion:start', (event, videos) => {
   const videoAry = _.values(videos);
-  console.log(videoAry);
-  const video = videoAry[0];
-  const outputDirectory = video.path.split(video.name)[0];
-  const outputName = video.name.split('.')[0];
-  const outputPath =`${outputDirectory}${outputName}.${video.format}`;
-  console.log(outputPath);
+  _.each(videoAry, video => {
+    const outputDirectory = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0];
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+    ffmepg(video.path)
+      .output(outputPath)
+      .on('end', () => console.log('Video conversion complete'))
+      .run();
+  });
 });
